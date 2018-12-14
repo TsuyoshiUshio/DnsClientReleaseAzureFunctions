c3637aabc810635bfecd60e7573b6382610ef851
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-12-14 15:36:48 +0900

    Initial Commit

--- AzureSearchDNSTesting/Function1.cs
+++ AzureSearchDNSTesting/Function1.cs
@@ -1,33 +1,167 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace AzureSearchDNSTesting
 {
+    public class DelegatingHandlerImpl : DelegatingHandler
+    {
+        public DelegatingHandlerImpl(HttpMessageHandler hanlder) : base(hanlder)
+        {
+
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response =  await base.SendAsync(request, cancellationToken);
+            return response;
+        }
+    }
+
+
+    class MyEventListener : EventListener
+    {
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            var memberNameIndex = eventData.PayloadNames.IndexOf("memberName");
+
+            var message = new StringBuilder();
+            for (var i = 0; i < eventData.Payload.Count; i++)
+            {
+                if (i == memberNameIndex) continue;
+                if (i > 0)
+                {
+                    message.Append(", ");
+                }
+
+                message.Append(eventData.PayloadNames[i] + "=" + eventData.Payload[i]);
+            }
+
+            var last = eventData.Payload.Last().ToString();
+
+            if (string.IsNullOrWhiteSpace(last)) return;
+            var content = message.ToString();
+            if (content.Contains("devopshackfestfuji.simplearchitect.club"))
+                Debug.WriteLine(message);
+        }
+    }
+
+    public class KeepAliveAvailableHttpClientHandler : HttpClientHandler
+    {
+        private readonly FieldInfo _fieldInfo =
+            typeof(HttpClientHandler).GetField("_socketsHttpHandler", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public KeepAliveAvailableHttpClientHandler()
+        {
+            var socketHttpHandler = (SocketsHttpHandler) _fieldInfo.GetValue(this);
+            socketHttpHandler.PooledConnectionIdleTimeout = TimeSpan.FromMinutes(3);
+            socketHttpHandler.PooledConnectionLifetime = TimeSpan.FromMinutes(1);
+        }
+    }
+
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        private static SearchIndexClient indexClient;
+        private static bool IsClearKeepAlive;
+
+        static Function1()
+        {
+
+            var netEventSource = EventSource.GetSources().FirstOrDefault(es => es.Name == "Microsoft-System-Net-Http");
+
+            if (netEventSource != null)
+            {
+                var myEventListener = new MyEventListener();
+
+                myEventListener.EnableEvents(netEventSource, EventLevel.LogAlways);
+            }
+
+            string searchServiceName = Environment.GetEnvironmentVariable("SearchServiceName");
+            string adminApiKey = Environment.GetEnvironmentVariable("SearchServiceAdminApiKey");
+            //ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) =>
+            //{
+            //    return true;
+            //};
+            var handler = new KeepAliveAvailableHttpClientHandler();
+            
+            handler.ServerCertificateCustomValidationCallback = (sender, certificate, chain, sslPolicyErrors) =>
+            {
+                return true;
+            };
+            var delegatingHanlder = new DelegatingHandlerImpl(handler);
+            var hanlders = new DelegatingHandler[]
+            {
+                delegatingHanlder
+            };
+
+
+            indexClient = new SearchIndexClient(searchServiceName, "azuresql-index", new SearchCredentials(adminApiKey), handler , hanlders);
+        }
+
+        [FunctionName("Search")]
+        public static async Task<IActionResult> Search(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            List<string> searchFields = new List<string>();
+            searchFields.Add("Name");
 
-            string name = req.Query["name"];
+            return new OkObjectResult(await SearchAsync("*", searchFields));
+         }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+        public static async Task<IEnumerable<Product>> SearchAsync(string query, List<string> searchFields)
+        {
+            var parameters = new SearchParameters();
+            parameters.SearchFields = searchFields;
+            indexClient.SearchDnsSuffix = "simplearchitect.club/func";
+            var results = await indexClient.Documents.SearchAsync<Product>(query, parameters);
+            
+            return results.Results.Select<SearchResult<Product>, Product>(p => p.Document);
+        }
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        [FunctionName("RefleshDNS")]
+        public static async Task<IActionResult> Reflesh(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req,
+            ILogger log)
+        {
+            // This doesn't work for .Net Core
+            //ServicePointManager.FindServicePoint(new Uri("https://devopshackfestfuji.simplearchitect.club")).ConnectionLeaseTimeout =
+            //    (int) TimeSpan.FromMinutes(1).TotalMilliseconds;
+            //ServicePointManager.DnsRefreshTimeout = (int) TimeSpan.FromMinutes(1).TotalMilliseconds;
+
+            return new OkObjectResult("DNS cache has been cleared.");
+        }
+
+        public class KeepAliveDisposalHttpClientHandler : HttpClientHandler
+        {
+            protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                if (IsClearKeepAlive)
+                {
+                    request.Headers.Add("Connection", "close");
+                }
+                return await base.SendAsync(request, cancellationToken);
+            }
         }
     }
 }
---
+++ AzureSearchDNSTesting/Models.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureSearchDNSTesting
+{
+
+    public class Rootobject
+    {
+        public Product[] Property1 { get; set; }
+    }
+
+    public class Product
+    {
+        public int searchscore { get; set; }
+        public string ProductID { get; set; }
+        public string Name { get; set; }
+        public string ProductNumber { get; set; }
+        public string Color { get; set; }
+        public string StandardCost { get; set; }
+        public string ListPrice { get; set; }
+        public string Size { get; set; }
+        public string Weight { get; set; }
+        public int ProductCategoryID { get; set; }
+        public int ProductModelID { get; set; }
+        public DateTime SellStartDate { get; set; }
+        public DateTime? SellEndDate { get; set; }
+        public object DiscontinuedDate { get; set; }
+        public string ThumbnailPhotoFileName { get; set; }
+        public string rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+
+}
